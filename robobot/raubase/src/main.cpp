95acec0ef2a3a3d373f78026ffc8abb1d15c95cc
xzores <jakobfu@hotmail.com>
2024-02-27 16:44:12 +0100

    Update main.cpp

--- robobot/raubase/src/main.cpp
+++ robobot/raubase/src/main.cpp
@@ -79,8 +79,8 @@ void go_for (float meters) {
 
 		dist = sqrt((start[0] - pose.x)*(start[0] - pose.x) + (start[1] - pose.y)*(start[1] - pose.y));
 		
-		float dir = abs(max_acc * time_interval) / (max_acc * time_interval);
-		cur_vel += fmin(target_vel - cur_vel, abs(max_acc * time_interval)) * dir;
+		float dir = abs(target_vel - cur_vel) / (target_vel - cur_vel);
+		cur_vel += fmin(abs(target_vel - cur_vel), max_acc * time_interval) * dir;
 		cur_vel = fmax(min_vel, cur_vel);
 
 		//The distance it will take to reach 0 m/s. A dist_margin is added so it can slow down beforehand.
