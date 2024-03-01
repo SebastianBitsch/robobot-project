a456bece69b1085bc6148fb227aed2894483ed3c
xzores <jakobfu@hotmail.com>
2024-03-01 15:03:10 +0100

    Update main.cpp

--- robobot/raubase/src/main.cpp
+++ robobot/raubase/src/main.cpp
@@ -111,7 +111,7 @@ void go_for (float meters, bool follow_line) {
 		
 		float left_sum = (float)left_sum_int;
 		float right_sum = (float)right_sum_int;
-		
+
 		heading += (left_sum - right_sum) * heading_kp; 
 
 		mixer.setDesiredHeading(heading);
@@ -120,6 +120,7 @@ void go_for (float meters, bool follow_line) {
 		float time_interval_usec = time_interval * 1000.0f * 1000.0f;
 		usleep((useconds_t)time_interval_usec); //ms before updating velocity and heading
 		printf("dist, cur_vel, target_vel,  %f, %f, %f\n", dist, cur_vel, target_vel);
+		printf("left_sum, right_sum, heading,  %f, %f, %f\n", left_sum, right_sum, heading);
 		printf("edge raw : %i, %i, %i, %i, %i, %i, %i, %i \n", sedge.edgeRaw[0], sedge.edgeRaw[1], sedge.edgeRaw[2], sedge.edgeRaw[3], sedge.edgeRaw[4], sedge.edgeRaw[5], sedge.edgeRaw[6], sedge.edgeRaw[7]);
 
 		if (dist >= meters) {
