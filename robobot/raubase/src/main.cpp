5dcbfeecf0538438103c1b3dbf2fd92efcc042b5
xzores <jakobfu@hotmail.com>
2024-03-01 15:10:15 +0100

    Update main.cpp

--- robobot/raubase/src/main.cpp
+++ robobot/raubase/src/main.cpp
@@ -69,7 +69,7 @@ float time_interval = 0.05;
 float dist_margin = 0.03;
 float min_vel = 0.04;
 
-float heading_kp = 0.0001;
+float heading_kp = 0.00001;
 
 void go_for (float meters, bool follow_line) {
 	
@@ -104,7 +104,7 @@ void go_for (float meters, bool follow_line) {
 		}
 		
 		cur_vel = fmax(min_vel, cur_vel);
-		
+
 		mixer.setVelocity(cur_vel);
 		
 		///////////////////////// Heading calculation /////////////////////////
@@ -115,7 +115,7 @@ void go_for (float meters, bool follow_line) {
 		float left_sum = (float)left_sum_int;
 		float right_sum = (float)right_sum_int;
 
-		heading += (right_sum - left_sum) * heading_kp; 
+		heading += (right_sum - left_sum) * heading_kp;
 
 		mixer.setDesiredHeading(heading);
 
