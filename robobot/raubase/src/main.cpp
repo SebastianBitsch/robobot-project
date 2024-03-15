284d177b500c1c17fcbb6f18251ff42751c99eea
xzores <jakobfu@hotmail.com>
2024-03-15 16:00:14 +0100

    Update main.cpp

--- robobot/raubase/src/main.cpp
+++ robobot/raubase/src/main.cpp
@@ -75,7 +75,7 @@ int main (int argc, char **argv)
 		gpio.setPin(16, 1);
 		{
 			auto p = furbs.vel;
-			furbs.go_for(3.73, left_line_mode, p);
+			furbs.go_for(-3.73, left_line_mode, p);
 			furbs.go_for(0.50, no_line_mode, p);
 			p.max_vel -= 0.1; //slow down a bit
 			furbs.go_for(2, left_line_mode, p);
@@ -118,7 +118,7 @@ int main (int argc, char **argv)
 		gpio.setPin(16, 0);
 	
 	}
-	
+
 	// close all logfiles etc.
 	service.terminate();
 	return service.theEnd;
