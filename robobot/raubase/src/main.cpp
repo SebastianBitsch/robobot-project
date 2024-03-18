c9f9f3eb780265e9be0a1590c3327e1d603046f7
xzores <jakobfu@hotmail.com>
2024-03-18 18:00:00 +0100

    Update main.cpp

--- robobot/raubase/src/main.cpp
+++ robobot/raubase/src/main.cpp
@@ -145,6 +145,95 @@ int main (int argc, char **argv)
 	return service.theEnd;
 }
 
+/*
+[service]
+use_robot_hardware = true
+
+[edge]
+rate_ms = 8
+highpower = true
+lograw = true
+printraw = false
+calibwhite = 0 0 1 0 1 0 0 0
+calibblack = 25 28 31 34 35 33 28 29
+whitethreshold = 500
+sensorwidth = 0.14
+log = true
+lognorm = true
+print = false
+kp = 85.0
+lead = 0.2 1.2
+taui = 0.00001
+logcedge = true
+logctrl = false
+printctrl = false
+maxturnrate = 5.0
 
 
+[dist]
+rate_ms = 20
+ir13cm = 47870 70000
+ir50cm = 15911 20000
+uscalib = 0.00126953125
+log = true
+print = false
+sensor1 = sharp
+sensor2 = sharp
 
+[postion]
+max_acc = 0.6
+max_vel = 0.35
+time_interval = 0.05
+dist_margin = 0.03
+min_vel = 0.05
+heading_vel = 0.05
+heading_threshold = 50
+heading_buildup_remove = 0.5
+
+[heading]
+kp = 65.0
+lead = 0.5 1.0
+taui = 0.000001
+maxturnrate = 3.0
+log = true
+print = false
+
+
+[motor]
+kp = 8.0
+lead = 0.0 1.0
+taui = 0.05
+maxmotv = 10.0
+log = true
+print_m1 = false
+print_m2 = false
+
+[encoder]
+rate_ms = 8
+log = true
+print = false
+encoder_reversed = true
+
+[pose]
+gear = 19.0
+wheeldiameter = 0.146
+enctickperrev = 64
+wheelbase = 0.28
+log = true
+print = false
+
+[camera]
+device = 0
+width = 1280
+height = 720
+fps = 25
+matrix = 1000 0 640 0 1000 360 0 0 1
+distortion = -0.415 0.2244 -6.875e-5 0.001279 -0.073412
+imagepath = img
+imagename = %d
+log = true
+print = false
+enabled = true
+pos = 0.11 0 0.23
+cam_tilt = 0.01
+*/
